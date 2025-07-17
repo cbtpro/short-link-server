dfc8c24e5d80e5473134350d24f6f2dc2b79f051
chenbitao <cbtpro@gmail.com>
2025-07-17 14:50:57 +0800

    feat: 🎸 add SkipEncryptionInterceptor

---
+++ src/common/decorator/skip-encryption-interceptor.decorator.ts
@@ -0,0 +1,5 @@
+import { SetMetadata } from '@nestjs/common';
+
+export const SKIP_ENCRYPTION_INTERCEPTOR_KEY = 'skipEncryptionInterceptor';
+
+export const SkipEncryptionInterceptor = () => SetMetadata(SKIP_ENCRYPTION_INTERCEPTOR_KEY, true);
--- src/interceptor/encryption.interceptor.ts
+++ src/interceptor/encryption.interceptor.ts
@@ -21,12 +21,28 @@ import {
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { EncryptionService } from '@/services/encryption.service';
+import { SKIP_ENCRYPTION_INTERCEPTOR_KEY } from '@/common/decorator/skip-encryption-interceptor.decorator';
+import { Reflector } from '@nestjs/core';
 
 @Injectable()
 export default class EncryptionInterceptor implements NestInterceptor {
-  constructor(private readonly encryptionService: EncryptionService) {}
+  constructor(private readonly encryptionService: EncryptionService, private reflector: Reflector) { }
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    /**
+     * 跳过加密拦截
+     */
+    const skip = this.reflector.getAllAndOverride<boolean>(
+      SKIP_ENCRYPTION_INTERCEPTOR_KEY,
+      [
+        context.getHandler(), // 方法上的元数据
+        context.getClass(),   // 控制器上的元数据
+      ],
+    );
+
+    if (skip) {
+      return next.handle(); // 跳过拦截逻辑
+    }
     const request = context.switchToHttp().getRequest();
     const { body } = request;
     if (body && body.ciphertext && typeof body.ciphertext === 'string') {
